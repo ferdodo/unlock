6dff47167706b0e5bd5f9b3a7ec5f8b93ae1b9d3
Thomas Riffard <ferdodo@hotmail.fr>
2023-12-01 22:32:58 +0100

    use WebAssembly in the project

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,45 +1,6 @@
-import { createApp, ref, Ref } from "vue";
-import { render } from "./template";
-import { Playground } from "./components/playground";
-import { puzzle$, puzzleUnresolved } from "./puzzle";
-import { getMoveCount } from "./moveCount";
-import "cookies-ds";
+import init from "wasm/pkg/unlock_puzzle_generator";
 
-export const app = createApp({
-	components: {
-		Playground
-	},
-	setup() {
-		const win: Ref<boolean> = ref(false);
-
-		puzzle$.subscribe(function(puzzle) {
-			if (!puzzleUnresolved(puzzle)) {
-				win.value = true;
-			}
-		});
-
-		function share() {
-			const date = new Date();
-			const year = date.getFullYear();
-			const month = ('0' + (date.getMonth() + 1)).slice(-2);
-			const day = ('0' + date.getDate()).slice(-2);
-			const formattedDate = `${year}/${month}/${day}`;
-			let text = `Unlock ${formattedDate}`;
-			const moveCount = getMoveCount();
-
-			text += `\n\nPuzzle réussi en ${ moveCount } mouvements.`
-
-			text += `\n\nhttps://ferdodo.github.io/unlock`;
-			navigator.clipboard.writeText(text);
-		}
-
-	
-		return { 
-			win,
-			share
-		};
-	},
-	render
-});
-
-app.mount("body");
+init(new URL('unlock_puzzle_generator_bg.wasm', document.location.href).toString())
+	.then(async function() {
+		await import("./initApp.js")
+	});
