714ac63f25a4778237dcceea07906c8e81af3aa9
Thomas Riffard <ferdodo@hotmail.fr>
2023-12-17 13:13:58 +0100

    move initApp to kebab-case

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -2,5 +2,5 @@ import init from "wasm/pkg/unlock_puzzle_generator";
 
 init(new URL('unlock_puzzle_generator_bg.wasm', document.location.href).toString())
 	.then(async function() {
-		await import("./initApp.js")
+		await import("./init-app.js")
 	});
