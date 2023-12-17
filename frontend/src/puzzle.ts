d11519a1ff3be58628ca33b735f2369f10623600
Thomas Riffard <ferdodo@hotmail.fr>
2023-12-17 12:15:32 +0100

    unimplement moving logic

--- frontend/src/puzzle.ts
+++ frontend/src/puzzle.ts
@@ -1,9 +1,6 @@
 import { Observable, share } from "rxjs";
 import { generatePuzzle } from "./generatePuzzle";
 import { Block } from "./Block";
-import { bitMoved$ } from "./bitMoved";
-import { latchMoved$ } from "./latchMoved";
-import { incrementMoveCount } from "unlock/move-count";
 import { Bit } from "unlock/bit";
 
 export interface Latch {
@@ -26,21 +23,7 @@ export function puzzleUnresolved(puzzle: Puzzle): boolean {
 	return puzzle.latch.block.x !== puzzle.block.w - puzzle.latch.block.w;
 }
 
-export const puzzle$: Observable<Puzzle> = new Observable(function(subscriber) {
-	bitMoved$.subscribe(function(movedBit: Bit) {
-		const bits = puzzle.bits.map(bit => (bit.id === movedBit.id) ? movedBit : bit);
-		incrementMoveCount();
-		puzzle = { ...puzzle, bits };
-		subscriber.next(puzzle);
-	});
-
-	latchMoved$.subscribe(function(movedLatch: Latch) {
-		puzzle = { ...puzzle, latch: movedLatch };
-		incrementMoveCount();
-		subscriber.next(puzzle);
-	});
-
-	subscriber.next(puzzle);
+export const puzzle$: Observable<Puzzle> = new Observable(function() {
 }).pipe(share<Puzzle | unknown>()) as Observable<Puzzle>;
 
 export function getBit(id: number): Bit {
