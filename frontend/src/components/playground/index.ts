643cf55cedc1d609470baa8f8ff060c6c18c4129
Thomas Riffard <ferdodo@hotmail.fr>
2023-12-17 12:53:35 +0100

    unimplement drag and drop logic

--- frontend/src/components/playground/index.ts
+++ frontend/src/components/playground/index.ts
@@ -1,12 +1,8 @@
 import { Ref, ref, defineComponent } from "vue";
 import { render } from "./template";
 import { BitComponent } from "../bit";
-import { DragDirection, dragDirection } from "../../dragDirection";
 import { LatchComponent } from "../latch";
-import { moveLatchEvents$ } from "../../moveLatchEvent";
-import { moveBitEvents$ } from "../../moveBitEvent";
 import { Puzzle } from "unlock/puzzle";
-import { Bit } from "../../bit";
 import { currentPuzzle$, getCurrentPuzzle } from "unlock/current-puzzle";
 
 export const Playground = defineComponent({
@@ -18,50 +14,8 @@ export const Playground = defineComponent({
 		const puzzle: Ref<Puzzle> = ref(getCurrentPuzzle());
 		currentPuzzle$.subscribe(p => puzzle.value = p);
 
-		function drag(event: DragEvent, bit: Bit) {
-			const direction: DragDirection | undefined = dragDirection(event);
-
-			if (direction !== undefined) {
-				switch (direction) {
-					case DragDirection.Up:
-						moveBitEvents$.next({ id: bit.id, x: bit.block.x, y: bit.block.y -1 });
-						break;
-					case DragDirection.Down:
-						moveBitEvents$.next({ id: bit.id, x: bit.block.x, y: bit.block.y +1 });
-						break;
-					case DragDirection.Right:
-						moveBitEvents$.next({ id: bit.id, x: bit.block.x +1, y: bit.block.y });
-						break;
-					case DragDirection.Left:
-						moveBitEvents$.next({ id: bit.id, x: bit.block.x -1, y: bit.block.y });
-						break;
-					default:
-						break;
-				}
-			}
-		}
-
-		function dragLatch(event: DragEvent) {
-			const direction: DragDirection | undefined = dragDirection(event);
-
-			if (direction !== undefined) {
-				switch (direction) {
-					case DragDirection.Right:
-						moveLatchEvents$.next({ x: puzzle.value.latch.block.x +1 });
-						break;
-					case DragDirection.Left:
-						moveLatchEvents$.next({ x: puzzle.value.latch.block.x -1 });
-						break;
-					default:
-						break;
-				}
-			}
-		}
-
 		return {
-			puzzle,
-			drag,
-			dragLatch
+			puzzle
 		};
 	},
 	render
