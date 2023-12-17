7b997be0ff85cf41bb68e3845e96559284a31e17
Thomas Riffard <ferdodo@hotmail.fr>
2023-12-17 12:55:42 +0100

    clean some files

--- frontend/src/moveBitEvent.ts
+++
@@ -1,9 +0,0 @@
-import { Subject } from "rxjs";
-
-export interface MoveBitEvent {
-	id: number;
-	x: number;
-	y: number;
-}
-
-export const moveBitEvents$: Subject<MoveBitEvent> = new Subject();
--- frontend/src/moveLatchEvent.ts
+++
@@ -1,7 +0,0 @@
-import { Subject } from "rxjs";
-
-export interface MoveLatchEvent {
-	x: number;
-}
-
-export const moveLatchEvents$: Subject<MoveLatchEvent> = new Subject();
